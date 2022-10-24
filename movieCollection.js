174301f7ecfebe88173d63ce9f1e9c895573d1f7
Antoine Rosenbach <github@rbch.ovh>
2022-10-24 14:12:05 +0200

    refacto: extract matches()

--- movieCollection.js
+++ movieCollection.js
@@ -1,8 +1,12 @@
+function matches(query, movie) {
+  return movie.title.includes(query);
+}
+
 function findByTitle(query, collection) {
   let result = [];
   let movie;
   while ((movie = collection.shift())) {
-    if (movie.title.includes(query)) {
+    if (matches(query, movie)) {
       result.push(movie);
     }
   }
