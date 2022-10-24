7cb4062087f780dd4e3caaf66e21d45afb5a1786
Antoine Rosenbach <github@rbch.ovh>
2022-10-24 13:48:25 +0200

    test(red): should return a matching movie

--- movieCollection.test.js
+++ movieCollection.test.js
@@ -22,4 +22,31 @@ describe("Movie collection search by name", () => {
     ]);
     expect(actual).toEqual([]);
   });
+
+  it("should return a matching movie", () => {
+    const actual = findByTitle("The Matrix", [
+      {
+        title: "The Matrix",
+        year: 1999,
+      },
+      {
+        title: "A Beautiful Mind",
+        year: 2001,
+      },
+      {
+        title: "Intouchable",
+        year: 2011,
+      },
+      {
+        title: "Forest Gump",
+        year: 1994,
+      },
+    ]);
+    expect(actual).toEqual([
+      {
+        title: "The Matrix",
+        year: 1999,
+      },
+    ]);
+  });
 });
