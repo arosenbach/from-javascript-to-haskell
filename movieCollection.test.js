ab84515fb5291575c890ed3d9382cd916432b1e5
Antoine Rosenbach <github@rbch.ovh>
2022-10-24 14:21:30 +0200

    !! refacto(test): extract constant `movies`

--- movieCollection.test.js
+++ movieCollection.test.js
@@ -1,47 +1,32 @@
 import findByTitle from "./movieCollection";
 
+const movies = [
+  {
+    title: "The Matrix",
+    year: 1999,
+  },
+  {
+    title: "A Beautiful Mind",
+    year: 2001,
+  },
+  {
+    title: "Intouchable",
+    year: 2011,
+  },
+  {
+    title: "Forest Gump",
+    year: 1994,
+  },
+];
+
 describe("Movie collection search by name", () => {
   it("should return empty when none found", () => {
-    const actual = findByTitle("Interstallar", [
-      {
-        title: "The Matrix",
-        year: 1999,
-      },
-      {
-        title: "A Beautiful Mind",
-        year: 2001,
-      },
-      {
-        title: "Intouchable",
-        year: 2011,
-      },
-      {
-        title: "Forest Gump",
-        year: 1994,
-      },
-    ]);
+    const actual = findByTitle("Interstallar", movies);
     expect(actual).toEqual([]);
   });
 
   it("should return a matching movie", () => {
-    const actual = findByTitle("The Matrix", [
-      {
-        title: "The Matrix",
-        year: 1999,
-      },
-      {
-        title: "A Beautiful Mind",
-        year: 2001,
-      },
-      {
-        title: "Intouchable",
-        year: 2011,
-      },
-      {
-        title: "Forest Gump",
-        year: 1994,
-      },
-    ]);
+    const actual = findByTitle("The Matrix", movies);
     expect(actual).toEqual([
       {
         title: "The Matrix",
@@ -51,24 +36,7 @@ describe("Movie collection search by name", () => {
   });
 
   it("should return all matching movies", () => {
-    const actual = findByTitle("o", [
-      {
-        title: "The Matrix",
-        year: 1999,
-      },
-      {
-        title: "A Beautiful Mind",
-        year: 2001,
-      },
-      {
-        title: "Intouchable",
-        year: 2011,
-      },
-      {
-        title: "Forest Gump",
-        year: 1994,
-      },
-    ]);
+    const actual = findByTitle("o", movies);
     expect(actual).toEqual([
       {
         title: "Intouchable",
