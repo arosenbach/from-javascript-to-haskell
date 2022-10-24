2f8f7946e215e4b9a9fba545829957f4ecca691a
Antoine Rosenbach <github@rbch.ovh>
2022-10-24 19:13:51 +0200

    refacto: add function signatures to find side-effects

--- movieCollection.js
+++ movieCollection.js
@@ -1,21 +1,27 @@
+// title :: Movie -> String
 function title(movie) {
   return movie.title;
 }
 
+// isInfixOf :: (String, String) -> Bool
 function isInfixOf(query, string) {
   return string.includes(query);
 }
 
+// matches :: (String, Movie) -> Bool
 function matches(query, movie) {
   return isInfixOf(query, title(movie));
 }
 
+// addIfMatches :: (String, Movie, [Movie]) -> ()
+// FIXME: side-effect
 function addIfMatches(query, movie, result) {
   if (matches(query, movie)) {
     result.push(movie);
   }
 }
 
+// findByTitle :: (String, [Movie]) -> [Movie]
 function findByTitle(query, collection) {
   let result = [];
   for (const movie of collection) {
