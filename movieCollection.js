6d5157ecdb5ed1c80e2482f8f71d7b302e6e03c2
Antoine Rosenbach <github@rbch.ovh>
2022-11-13 17:40:08 +0100

    pass the path as a parameter of `matches`

--- movieCollection.js
+++ movieCollection.js
@@ -1,9 +1,9 @@
 import * as R from "ramda";
 
-// matches :: String -> Movie -> Bool
-const matches = (query) => R.o(R.includes(query))(R.path(["title"]));
+// matches :: Path -> String -> Movie -> Bool
+const matches = (path) => (query) => R.o(R.includes(query))(R.path(path));
 
 // findByTitle :: String -> [Movie] -> [Movie]
-const findByTitle = R.o(R.filter)(matches);
+const findByTitle = R.o(R.filter)(matches(["title"]));
 
 export default findByTitle;
